cf9465cc04b819cc5da0159503298917f39916b5
Abdul-rasaq Wasiu <wasiu.abdulrasaq1@gmail.com>
2022-07-05 17:53:42 -0700

    update 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.c"
+/**
+ * main - Entry Point
+ *
+ * Return Always 0 (sucess)
+ */
+int main(void)
+{
+	char *h = "Alxtraining\n";
+	
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+
+	}
+	return (0);
+}	
+
